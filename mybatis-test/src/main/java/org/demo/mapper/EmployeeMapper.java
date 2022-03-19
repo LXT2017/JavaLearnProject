17853967ea3536a2009505feb53f70aedb4ecbd7
shawn <13626862366@163.com>
2022-03-19 20:12:29 +0800

    mybatis

--- mybatis-test/src/main/java/org/demo/mapper/EmployeeMapper.java
+++ mybatis-test/src/main/java/org/demo/mapper/EmployeeMapper.java
@@ -3,6 +3,7 @@ package org.demo.mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.cursor.Cursor;
 import org.demo.po.Employee;
 
 import java.util.List;
@@ -25,4 +26,13 @@ public interface EmployeeMapper {
     @Select("select * from employee where id = #{id}")
     @Options
     Employee getEmpById(@Param("id") Integer id);
+
+    // MySQL 默认情况下，创建 prepareStatement 时，就已经是 ResultSet.TYPE_FORWARD_ONLY 和 ResultSet.CONCUR_READ_ONLY ，所以这两个参数可加可不加
+    // @Select("select * from employee")
+    // @Options(resultSetType = ResultSetType.FORWARD_ONLY, fetchSize = Integer.MIN_VALUE)
+    // @ResultType(Employee.class)
+    // void getEmpList(ResultHandler<Employee> resultHandler);
+
+    @Select("select * from employee limit #{limit}")
+    Cursor<Employee> scan(@Param("limit") int limit);
 }
--- mybatis-test/src/main/java/org/demo/test/MybatisTest.java
+++ mybatis-test/src/main/java/org/demo/test/MybatisTest.java
@@ -1,15 +1,16 @@
 package org.demo.test;
 
+import org.apache.ibatis.cursor.Cursor;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.demo.mapper.DynamicSQLMapper;
+import org.demo.mapper.EmployeeMapper;
 import org.demo.po.Employee;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author Shawn
@@ -27,12 +28,15 @@ public class MybatisTest {
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         // 获取sqlSession对象
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        DynamicSQLMapper mapper = sqlSession.getMapper(DynamicSQLMapper.class);
+        EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
         // 执行数据库操作
-        Employee employee = new Employee();
-        employee.setId(1);
-        List<Employee> list = mapper.selectEmployeeBySql();
-        System.out.println(list);
+        AtomicInteger num = new AtomicInteger();
+        try(Cursor<Employee> cursor = mapper.scan(4)) {
+            cursor.forEach(e->{
+                System.out.println(e);
+            });
+        }
+        // System.out.println(empList);
         // List<Employee> list = mapper.getEmp1();
         // Department department1 = mapper.getDeptStep(1);
         // List<Employee> list = mapper.getEmp1();
